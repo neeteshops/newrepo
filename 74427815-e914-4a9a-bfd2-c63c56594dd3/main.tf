bd232152a2f8a09ed7995039934c91b66c49699b
neeteshops <Neetesh.Kumar@ops0.com>
2025-07-25 16:32:08 +0530

    IaC changes

--- 74427815-e914-4a9a-bfd2-c63c56594dd3/main.tf
+++ 74427815-e914-4a9a-bfd2-c63c56594dd3/main.tf
@@ -1,8 +1,22 @@
-module "testtttt12" {
-  source = "./testtttt12"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
 }
 
-module "nest111" {
-  source = "./nest111"
+provider "aws" {
+  region = var.region
 }
 
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.environment
+  }
+}
---
+++ 74427815-e914-4a9a-bfd2-c63c56594dd3/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {
+  description = "AWS region to deploy resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment (e.g. dev, prod, staging)"
+  type        = string
+  default     = "dev"
+}
