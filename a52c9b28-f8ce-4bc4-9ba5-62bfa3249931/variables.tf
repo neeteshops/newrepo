75105de6bde0d37bd34b926035f86d72524942fb
neeteshops <Neetesh.Kumar@ops0.com>
2025-07-28 08:26:24 +0530

    IaC changes

--- a52c9b28-f8ce-4bc4-9ba5-62bfa3249931/variables.tf
+++ a52c9b28-f8ce-4bc4-9ba5-62bfa3249931/variables.tf
@@ -1,5 +1,5 @@
 variable "region" {
-  description = "The AWS region to deploy resources"
+  description = "The AWS region to deploy resources in"
   type        = string
   default     = "us-east-1"
 }
@@ -7,11 +7,11 @@ variable "region" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "neetesh00099099"
+  default     = "neetesh-apply0998"
 }
 
 variable "environment" {
-  description = "Environment tag for the S3 bucket"
+  description = "The environment this bucket is for (e.g., dev, prod)"
   type        = string
   default     = "dev"
 }
