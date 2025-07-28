a48b4a40c05809e440eeacdd3d52d7804be028f5
neeteshops <Neetesh.Kumar@ops0.com>
2025-07-28 19:54:11 +0530

    IaC changes

--- a52c9b28-f8ce-4bc4-9ba5-62bfa3249931/main.tf
+++ a52c9b28-f8ce-4bc4-9ba5-62bfa3249931/main.tf
@@ -1,12 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
 resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
-  
-  tags = {
-    Name        = var.bucket_name
-    Environment = var.environment
-  }
 }
